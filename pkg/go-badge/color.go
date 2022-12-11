829e253437d4b08a1382926c857ea4278ae0ee2e
soulteary <soulteary@gmail.com>
2022-12-11 16:51:39 +0800

    chore: update color const with badges/shields

--- pkg/go-badge/color.go
+++ pkg/go-badge/color.go
@@ -4,6 +4,7 @@ package badge
 type Color string
 
 // ColorScheme contains named colors that could be used to render the badge.
+// https://github.com/badges/shields/blob/b6be37d277b64a90bde98ca10446c9d433a56681/badge-maker/lib/color.js#L6
 var ColorScheme = map[string]string{
 	"brightgreen": "#4c1",
 	"green":       "#97ca00",
@@ -13,9 +14,15 @@ var ColorScheme = map[string]string{
 	"red":         "#e05d44",
 	"blue":        "#007ec6",
 	"grey":        "#555",
-	"gray":        "#555",
 	"lightgrey":   "#9f9f9f",
-	"lightgray":   "#9f9f9f",
+	// aliases
+	"gray":          "#555",
+	"lightgray":     "#9f9f9f",
+	"critical":      "#e05d44",
+	"important":     "#fe7d37",
+	"success":       "#4c1",
+	"informational": "#007ec6",
+	"inactive":      "#9f9f9f",
 }
 
 // Standard colors.
@@ -28,9 +35,15 @@ const (
 	ColorRed         = Color("red")
 	ColorBlue        = Color("blue")
 	ColorGrey        = Color("grey")
-	ColorGray        = Color("gray")
 	ColorLightgrey   = Color("lightgrey")
-	ColorLightgray   = Color("lightgray")
+	// aliases
+	ColorGray          = Color("gray")
+	ColorLightgray     = Color("lightgray")
+	ColorCritical      = Color("critical")
+	ColorImportant     = Color("important")
+	ColorSuccess       = Color("success")
+	ColorInformational = Color("informational")
+	ColorInactive      = Color("inactive")
 )
 
 func (c Color) String() string {
